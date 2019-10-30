eecea5e493a58364cba45a93a68491dfb9fa912c
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2019-10-30 12:03:36 -0400

    Added color snad basic header html

--- web/app/themes/oa/resources/views/partials/header.blade.php
+++ web/app/themes/oa/resources/views/partials/header.blade.php
@@ -1,10 +1,40 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+  	<!-- hat nav-->
+  	<div class="row hat">
+  		<div class="col-md-4">
+  			country select
+  		</div>
+  		<div class="col-md-4">
+  			Language select
+  		</div>
+  		<div class="col-md-4">
+  			@if (has_nav_menu('header_navigation'))
+        		{!! wp_nav_menu(['theme_location' => 'header_navigation']) !!}
+        	@endif
+        </div>
+    </div>
+  	<!-- //END hat -->
+  	<!-- Brand and main nav row -->
+    <div class="row brand-nav">
+    	<div class="col-md-6">
+    		<a class="brand" href="{{ home_url('/') }}">
+    			@if (get_custom_logo())
+    				{!!  get_custom_logo() !!}
+    			@else
+    				{{ get_bloginfo('name', 'display') }}
+    			@endif
+    		</a>
+    		
+    	</div>
+    	<div class="col-md-6">
+		    <nav class="nav-primary">
+		      @if (has_nav_menu('primary_navigation'))
+		        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+		      @endif
+		    </nav>
+		</div>
+	</div>
+    <!-- //END -->
   </div>
 </header>
