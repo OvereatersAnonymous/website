8869a43697759ae842c615148f3af1116c408e8c
Al Sierra <al@cloudred.com>
2019-11-05 18:47:45 -0500

    Change class names

--- web/app/themes/oa/resources/views/partials/homepage-podcast.blade.php
+++ web/app/themes/oa/resources/views/partials/homepage-podcast.blade.php
@@ -1,11 +1,11 @@
 <div class="podcast">
     <div class="row">
         @if($podcast_callout['podcast_home_image'])
-        <div class="podcast--image">
+        <div class="podcast__image">
             <img src="{!! $podcast_callout['podcast_home_image']['sizes']['medium@1x'] !!}" alt="{!! $podcast_callout['podcast_home_image']['alt'] !!}" title="{!! $podcast_callout['podcast_home_image']['alt'] !!}" />
         </div>
         @endif
-        <div class="podcast--callout">
+        <div class="podcast__callout">
             @include('partials.content-callout',['callout'=>$podcast_callout])
         </div>
     </div>
--- web/app/themes/oa/resources/views/partials/homepage-signup.blade.php
+++ web/app/themes/oa/resources/views/partials/homepage-signup.blade.php
@@ -1,9 +1,9 @@
 <div class="signup">
     <div class="row">
-        <div class="signup--form">
+        <div class="signup__form">
 
         </div>
-        <div class="signup--callout">
+        <div class="signup__callout">
             @include('partials.content-callout',['callout'=>$signup_callout])
         </div>
     </div>
