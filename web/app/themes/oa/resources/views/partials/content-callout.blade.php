2be24e225e2a525f4d864b54de0716afa31d169e
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2019-11-07 10:45:08 -0500

    Added google translate widget

--- web/app/themes/oa/resources/views/partials/content-callout.blade.php
+++ web/app/themes/oa/resources/views/partials/content-callout.blade.php
@@ -3,7 +3,7 @@
     <div class="copy">{!! $callout['callout_copy'] !!}</div>
     @if($callout['button_url'] || $callout['internal_button_link'])
         <div class="button-container">
-            <a class="btn {!! $callout['button_style']!!}" href="@if ($callout['button_link_type']) == 1 {!! $callout['button_url']!!} @else {!! $callout['internal_button_link']!!} @endif">{!! $callout['button_text'] !!}</a>
+            <a class="btn {!! $callout['button_style']!!}" href="@if($callout['button_link_type'] == '1') {!! $callout['button_url']!!} @else {!! $callout['internal_button_link']!!} @endif">{!! $callout['button_text'] !!}</a>
         </div>
     @endif
 </div>
