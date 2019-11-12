419f8fc6b43df5a4cbf9aed6f12939a63c2dd39a
Al Sierra <al@cloudred.com>
2019-11-12 13:19:22 -0500

    Add redirects code lib to manage any redirects and altering wordpress permalinks defintions set up podcast pagination permalink defintion update tab/podcast listing to reflect changes above

---
+++ web/app/themes/oa/app/redirects.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Actions/Filter functions for code related to redirects
+ */
+
+namespace App;
+
+/**
+ * Add rewrite rule for podcasts listing pretty url
+ *
+ * @hook rewrite_rules_array
+ * @return array
+ */
+function oa_add_rewrite_rules_podcasts($aRules) {
+	$aNewRules = array('podcasts/page/?([0-9]{1,})/?$' => 'index.php?pagename=podcasts&paged=$matches[1]');
+	$aRules = $aNewRules + $aRules;
+	return $aRules;
+}
+add_filter('rewrite_rules_array', 'App\\oa_add_rewrite_rules_podcasts');
