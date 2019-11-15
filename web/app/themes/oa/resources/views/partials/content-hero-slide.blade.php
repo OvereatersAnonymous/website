e0c3cdbb27c227db08e64815f4132064e7ca30de
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2019-11-15 18:19:07 -0500

    Updated call to remove error on slider button

--- web/app/themes/oa/resources/views/partials/content-hero-slide.blade.php
+++ web/app/themes/oa/resources/views/partials/content-hero-slide.blade.php
@@ -3,9 +3,9 @@
     <div class="callout">
         <div class="title">{!! $slide['hero_home_callout_title'] !!}</div>
         <div class="copy">{!! $slide['hero_home_callout_copy'] !!}</div>
-        @if($slide['hero_home_callout_button_button_url'])
+        @if($slide['hero_home_button_url'])
         <div class="button-container">
-            <a class="button" href="{!! $slide['hero_home_callout_button_button_url']!!}">{!! $slide['hero_home_callout_button_button_text'] !!}</a>
+            <a class="button" href="{!! $slide['hero_home_button_url']!!}">{!! $slide['hero_home_callout_button_button_text'] !!}</a>
         </div>
         @endif
     </div>
