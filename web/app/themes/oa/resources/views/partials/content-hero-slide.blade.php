37528e10b2d886e8b196f791d00116e09b79b0b4
Al Sierra <al@cloudred.com>
2019-11-15 19:47:07 -0500

    Fix button references due to changes to acf pro form

--- web/app/themes/oa/resources/views/partials/content-hero-slide.blade.php
+++ web/app/themes/oa/resources/views/partials/content-hero-slide.blade.php
@@ -3,10 +3,10 @@
     <div class="callout">
         <div class="title">{!! $slide['hero_home_callout_title'] !!}</div>
         <div class="copy">{!! $slide['hero_home_callout_copy'] !!}</div>
-        @if($slide['hero_home_callout_button_button_url'])
-        <div class="button-container">
-            <a class="button" href="{!! $slide['hero_home_callout_button_button_url']!!}">{!! $slide['hero_home_callout_button_button_text'] !!}</a>
-        </div>
+        @if($slide['hero_home_button_url'] || $slide['hero_home_internal_button_link'])
+            <div class="button-container">
+                <a class="btn {!! $slide['hero_home_button_style']!!}" href="@if($slide['hero_home_button_link_type'] == '1') {!! $slide['hero_home_button_url']!!} @else {!! $slide['hero_home_internal_button_link']!!} @endif">{!! $slide['hero_home_button_text'] !!}</a>
+            </div>
         @endif
     </div>
 </div>
