fe010b71ea527af8bdb0a79db5320ebfebcec1df
Al Sierra <al@cloudred.com>
2019-11-18 19:22:17 -0500

    Add Cookiebot JS script in tracker template

--- web/app/themes/oa/resources/views/partials/trackers.blade.php
+++ web/app/themes/oa/resources/views/partials/trackers.blade.php
@@ -8,3 +8,7 @@
     //gtag('config', '10184600-1');
 </script>
 <!-- End Global site tag (gtag.js) - Google Analytics -->
+<!-- Cookiebot for GDPR compliance and Cookie tracking -->
+<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="9044c0f0-589c-41fb-8fde-5910024179ea" data-blockingmode="auto" type="text/javascript"></script>
+<script id="CookieDeclaration" src="https://consent.cookiebot.com/9044c0f0-589c-41fb-8fde-5910024179ea/cd.js" type="text/javascript" async></script>
+<!-- End Cookiebot -->
