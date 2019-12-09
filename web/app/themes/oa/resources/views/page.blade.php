45e3bd0d08cee6fd12dfb10295db34b5959d57d9
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2019-12-09 10:41:29 -0500

    Styled secondary page layout

--- web/app/themes/oa/resources/views/page.blade.php
+++ web/app/themes/oa/resources/views/page.blade.php
@@ -14,7 +14,14 @@
 @endsection
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
+  	<div class="row">
+  		<div class="col-md-2"></div>
+  		<div class="col-md-8">
+	    	@include('partials.page-header')
+	    	@include('partials/content-share')
+	    	@include('partials.content-page')
+	    </div>
+	    <div class="col-md-2"></div>
+    </div>
   @endwhile
 @endsection
