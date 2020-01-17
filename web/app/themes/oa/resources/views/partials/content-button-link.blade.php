845fcc1a20e7328166fba798a608cad50d314bf7
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2020-01-17 12:38:53 -0500

    Styled page btn link, updated os configs tag, added bg color selection to pages and posts

--- web/app/themes/oa/resources/views/partials/content-button-link.blade.php
+++ web/app/themes/oa/resources/views/partials/content-button-link.blade.php
@@ -1,7 +1,7 @@
 @if(!empty(App::get_field('button_link_url')))
 <div class="button-link">
   <div class="button-container">
-    <a class="btn" href="{!! App::get_field('button_link_url') !!}" target="@if(!empty(App::get_field('button_link_target'))){!! App::get_field('button_link_target') !!}@else _parent @endif"> @if(!empty(App::get_field('button_link_icon')))<i class="{!! App::get_field('button_link_icon') !!}"></i>@endif @if(!empty(App::get_field('button_link_text'))){!! App::get_field('button_link_text') !!}@endif</a>
+    <a class="btn small smoky" href="{!! App::get_field('button_link_url') !!}" target="@if(!empty(App::get_field('button_link_target'))){!! App::get_field('button_link_target') !!}@else _parent @endif"> @if(!empty(App::get_field('button_link_icon')))<i class="{!! App::get_field('button_link_icon') !!}"></i>@endif @if(!empty(App::get_field('button_link_text'))) &nbsp;{!! App::get_field('button_link_text') !!}@endif</a>
   </div>
 </div>
 @endif
