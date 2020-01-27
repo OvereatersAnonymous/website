f55f05be4fefc599d663aeadcc2450749d00e75f
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2020-01-27 12:56:18 -0500

    Re-enable podcast image in mobile view

--- web/app/themes/oa/resources/views/partials/homepage-podcasts.blade.php
+++ web/app/themes/oa/resources/views/partials/homepage-podcasts.blade.php
@@ -2,12 +2,8 @@
     <div class="jumbotron-fluid podcasts">
         <div class="container">
             <div class="row podcasts--podcast">
-                <div class="col-md-3">
-                    @if($podcast_callout['podcast_home_image'])
-                    <div class="podcasts--image">
-                        <img src="{!! $podcast_callout['podcast_home_image']['sizes']['medium'] !!}" data-src="{!! $podcast_callout['podcast_home_image']['sizes']['medium'] !!}|{!! $podcast_callout['podcast_home_image']['sizes']['medium@2x'] !!}" alt="{!! $podcast_callout['podcast_home_image']['alt'] !!}"  />
-                    </div>
-                    @endif
+                <div class="col-md-3 unveil-bg" data-src="{!! $podcast_callout['podcast_home_image']['sizes']['medium'] !!}|{!! $podcast_callout['podcast_home_image']['sizes']['medium@2x'] !!}">
+                    <span class="sr-only">{!! $podcast_callout['podcast_home_image']['alt'] !!}</span>
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-8 podcasts--callout">
